43232775320ab10c44b98fe246003af44eb4cefe
ptmonline <ptmonlinework@gmail.com>
2019-04-03 10:08:01 +0200

    added training page squeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'training',
+    loadChildren: './training/training.module#TrainingPageModule'
   }
 ];
 
---
+++ src/app/training/training.page.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-training-page',
+    templateUrl: 'training.page.html',
+    styleUrls: ['training.page.scss']
+})
+
+export class TrainingPage{
+    constructor(){}
+}
